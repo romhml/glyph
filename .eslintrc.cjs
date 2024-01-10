652f42e53c207f521c8704adb3361629178db374
Romain Hamel <rom.hml@gmail.com>
2024-01-10 15:26:39 +0100

    chore: add commitlint config and fix eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
--- apps/extension/.eslintrc.cjs
+++ apps/extension/.eslintrc.cjs
@@ -2,7 +2,6 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
